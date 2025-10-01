6506cfb2cc539cb0cd3638fe7a463c0e879a85d9
shft <101985835+xhft@users.noreply.github.com>
2025-09-30 23:00:40 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -64,11 +64,11 @@ export const profileConfig: ProfileConfig = {
 			// `pnpm add @iconify-json/<icon-set-name>`
 			url: "https://twitter.com",
 		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
+		// {
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
+		// },
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
