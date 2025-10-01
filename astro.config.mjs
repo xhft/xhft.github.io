a1ab902ad5afe0e8ddd6f42f6de187e382ce5956
Rae Onhold <rae@example.com>
2025-09-30 22:01:54 -0300

    yehw

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://florescence.github.io/",
+	base: "/florescence",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
