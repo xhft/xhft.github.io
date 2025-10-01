70ea808b7e5e680f7449eeccb591974233e73931
shft <101985835+xhft@users.noreply.github.com>
2025-09-30 22:58:55 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "welcome to my madness",
-	subtitle: "portfolio",
+	title: "Blog",
+	subtitle: "a little research",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
