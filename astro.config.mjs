76d5e95d57de180645e6b732464fbbf1108e08ba
shft <101985835+xhft@users.noreply.github.com>
2025-09-30 22:45:24 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://xhft.github.io/",
-	base: "/",
+	base: "florescence.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
