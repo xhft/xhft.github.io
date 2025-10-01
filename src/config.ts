9aa2d9bd849734a0251a2e6da22293fb78d0660e
shft <101985835+xhft@users.noreply.github.com>
2025-09-30 22:44:26 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "shft",
-	subtitle: "shft portfolio",
+	subtitle: "portfolio",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
